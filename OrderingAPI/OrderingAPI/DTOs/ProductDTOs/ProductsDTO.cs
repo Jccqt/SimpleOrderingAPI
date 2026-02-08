8760555ee179232fde4236b5d2746d67f30ad0f2
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-02-08 19:55:36 +0800

    Feat (Product): Added Product model and ProductsDTO

---
+++ OrderingAPI/OrderingAPI/DTOs/ProductDTOs/ProductsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderingAPI.DTOs.ProductDTOs
+{
+    public class ProductsDTO
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public decimal price { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ OrderingAPI/OrderingAPI/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderingAPI.Models
+{
+    public class Products
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public decimal price { get; set; }
+        public int stock { get; set; }
+    }
+}
