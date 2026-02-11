89f327e2d3ad4f862120aa1756dbfafd469d2c98
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-02-11 20:13:05 +0800

    Refactor (Auth): Re-push LoginRequestDTO and IAuthRepository

---
+++ OrderingAPI/OrderingAPI/DTOs/AuthDTOs/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderingAPI.DTOs.AuthDTOs
+{
+    public class LoginRequestDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ OrderingAPI/OrderingAPI/Interfaces/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using OrderingAPI.DTOs.AuthDTOs;
+
+namespace OrderingAPI.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<bool> Login(LoginRequestDTO login);
+    }
+}
