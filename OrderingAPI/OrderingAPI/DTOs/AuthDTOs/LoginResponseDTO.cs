49a2659d5ea9cf7b92acc55ae199cc184538de32
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-02-12 19:50:28 +0800

    Docs: Added queries documentation for Views, Triggers, and Procedures

---
+++ OrderingAPI/OrderingAPI/DTOs/AuthDTOs/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderingAPI.DTOs.AuthDTOs
+{
+    public class LoginResponseDTO
+    {
+        public bool Success { get; set; }
+        public string? UserID { get; set; }
+        public string? FullName { get; set; }
+        public string? Role { get; set; }
+        public string? Token { get; set; }
+    }
+}
